87b37396f1a73edc81d2be74fe4da8223b9d4446
Ajmal Jalal <ajmal@zennify.com>
2020-11-25 21:40:04 -0800

    projects page design added

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,97 +1,56 @@
 import React, { Component } from 'react'
-import Project1 from '../assets/project-1.PNG'
-import Project2 from '../assets/project-2.PNG'
-import Project3 from '../assets/project-3.PNG'
-import Project4 from '../assets/project-4.PNG'
-import Project5 from '../assets/project-5.PNG'
-import Project6 from '../assets/project-6.PNG'
+import { projects } from './data'
 
 export default class Projects extends Component {
 
-  renderProjects = () => {
-    const projects = [
-      {
-        image: Project1,
-        title: 'Messages',
-        desc: 'This is a UI build with css and html for begginers. We will make a message UI using html and css',
-        cohort: 'html',
-        level: 'Beginner',
-        option: 'Required'
-      },
-      {
-        image: Project2,
-        title: 'Daria Nkitina',
-        desc: 'This is a UI build with css and html for Beginners. We will be making a profile UI for Daria to chat with her freinds',
-        cohort: 'html',
-        level: 'Beginner',
-        option: 'Required'
-      },
-      {
-        image: Project3,
-        title: 'Projects',
-        desc: 'This is a UI build with css and html for Beginners. This is a showcase of all the projects that a person will be building',
-        cohort: 'html',
-        level: 'Beginner',
-        option: 'Elective'
-      },
-      {
-        image: Project4,
-        title: 'OMac',
-        desc: 'This is a UI build with css and html for Beginners. This one is a medium level project with css and html for those who finishe the first one',
-        cohort: 'html',
-        level: 'Medium',
-        option: 'Required'
-      },
-      {
-        image: Project5,
-        title: 'Real Engines',
-        desc: 'This is a UI build with css and html for Beginners. Here we will be making real engines that is use for displaying real data',
-        cohort: 'html',
-        level: 'Advance',
-        option: 'Required'
-      },
-      {
-        image: Project6,
-        title: 'Dashboard',
-        desc: 'This is a UI build with css and html for Beginners. This is a black mode css and html web project for advance users',
-        cohort: 'html',
-        level: 'Advance',
-        option: 'Required'
-      },
-    ]
+  state = {
+    projects: 'html'
+  }
 
-    return projects.map((project, index) => {
-      return (
-        <div className='projects__list-item' key={index}>
-          <div className='list-item__screenshot'>
-            <img src={project.image} alt='project-screenshot' />
-          </div>
-          <div className='list-item__desc'>
-            <div className='list-item__title'>
-              {project.title}
-            </div>
-            <div className='list-item__desc-text'>
-              {project.desc}
-            </div>
-            <div className='list-item__tags'>
-              <div className='tags-item tags-item--green'>{project.cohort}</div>
-              <div className='tags-item tags-item--orange'>{project.level}</div>
-              <div className='tags-item tags-item--red'>{project.option}</div>
-            </div>
-          </div>
-        </div>
-      )
+  setCurrentProjectsTab = (tab) => {
+    this.setState({
+      projects: tab
     })
   }
+  renderProjects = () => {
+
+    return projects.filter(project => {
+      return project.cohort === this.state.projects
+    })
+      .map((project, index) => {
+        return (
+          <div className='projects__list-item' key={index}>
+            <div className='list-item__screenshot'>
+              <img src={project.image} alt='project-screenshot' />
+            </div>
+            <div className='list-item__desc'>
+              <div className='list-item__title'>
+                {project.title}
+              </div>
+              <div className='list-item__desc-text'>
+                {project.desc}
+              </div>
+              <div className='list-item__tags'>
+                <div className='tags-item tags-item--green'>{project.cohort}</div>
+                <div className='tags-item tags-item--orange'>{project.level}</div>
+                <div className='tags-item tags-item--red'>{project.option}</div>
+              </div>
+              <div className='list-item__preview'><p>Preview project</p></div>
+            </div>
+          </div>
+        )
+      })
+  }
   render() {
+    const { projects } = this.state
     return (
       <div className='projects-w'>
         <div className='projects__header'>
           <div className='projects__header__btns-w'>
-            <div className='projects__header__btn'>HTML & CSS</div>
-            <div className='projects__header__btn'>Vanila JavaScript</div>
-            <div className='projects__header__btn'>React & Redux</div>
-            <div className='projects__header__btn'>Node, Express & Databases</div>
+            <div className={projects === 'html' ? 'projects__header__btn projects__header__btn--border-bottom' : 'projects__header__btn'} onClick={() => this.setCurrentProjectsTab('html')}>HTML & CSS</div>
+            <div className={projects === 'javascript' ? 'projects__header__btn projects__header__btn--border-bottom' : 'projects__header__btn'} onClick={() => this.setCurrentProjectsTab('javascript')}>Vanila JavaScript</div>
+            <div className={projects === 'react' ? 'projects__header__btn projects__header__btn--border-bottom' : 'projects__header__btn'} onClick={() => this.setCurrentProjectsTab('react')}>React & Redux</div>
+            <div className={projects === 'node' ? 'projects__header__btn projects__header__btn--border-bottom' : 'projects__header__btn'} onClick={() => this.setCurrentProjectsTab('node')}>Node, Express & Databases</div>
           </div>
           <div>list view</div>
         </div>
---
+++ src/containers/data.js
@@ -0,0 +1,129 @@
+
+import Project1 from '../assets/project-1.PNG'
+import Project2 from '../assets/project-2.PNG'
+import Project3 from '../assets/project-3.PNG'
+import Project4 from '../assets/project-4.PNG'
+import Project5 from '../assets/project-5.PNG'
+import Project6 from '../assets/project-6.PNG'
+export const projects = [
+    {
+        image: Project1,
+        title: 'Messages',
+        desc: 'This is a UI build with css and html for begginers. We will make a message UI using html and css',
+        cohort: 'html',
+        level: 'Beginner',
+        option: 'Required'
+    },
+    {
+        image: Project2,
+        title: 'Cubic',
+        desc: 'This is a UI build with css and html for Beginners. We will be making a profile UI for Daria to chat with her freinds',
+        cohort: 'html',
+        level: 'Beginner',
+        option: 'Required'
+    },
+    {
+        image: Project3,
+        title: 'Game',
+        desc: 'This is a UI build with css and html for Beginners. This is a showcase of all the projects that a person will be building',
+        cohort: 'html',
+        level: 'Beginner',
+        option: 'Elective'
+    },
+    {
+        image: Project4,
+        title: 'Calculator',
+        desc: 'This is a UI build with css and html for Beginners. For real people and with real experience that wants to build more experience This one is a medium level project with css and html for those who finishe the first one',
+        cohort: 'html',
+        level: 'Medium',
+        option: 'Required'
+    },
+    {
+        image: Project5,
+        title: 'Dashboard',
+        desc: 'This is a UI build with css and html for Beginners. Here we will be making real engines that is use for displaying real data',
+        cohort: 'html',
+        level: 'Advance',
+        option: 'Required'
+    },
+    {
+        image: Project6,
+        title: 'Social Media',
+        desc: 'This is a UI build with css and html for Beginners. This is a black mode css and html web project for advance users',
+        cohort: 'html',
+        level: 'Advance',
+        option: 'Required'
+    },
+    {
+        image: Project6,
+        title: 'Social Media',
+        desc: 'This is a UI build with css and html for Beginners. This is a black mode css and html web project for advance users',
+        cohort: 'javascript',
+        level: 'Advance',
+        option: 'Required'
+    },
+    {
+        image: Project5,
+        title: 'Dashboard',
+        desc: 'This is a UI build with css and html for Beginners. Here we will be making real engines that is use for displaying real data',
+        cohort: 'javascript',
+        level: 'Advance',
+        option: 'Required'
+    },
+    {
+        image: Project4,
+        title: 'Calculator',
+        desc: 'This is a UI build with css and html for Beginners. For real people and with real experience that wants to build more experience This one is a medium level project with css and html for those who finishe the first one',
+        cohort: 'javascript',
+        level: 'Medium',
+        option: 'Required'
+    },
+    {
+        image: Project1,
+        title: 'Messages',
+        desc: 'This is a UI build with css and html for begginers. We will make a message UI using html and css',
+        cohort: 'node',
+        level: 'Beginner',
+        option: 'Required'
+    },
+    {
+        image: Project2,
+        title: 'Cubic',
+        desc: 'This is a UI build with css and node for Beginners. We will be making a profile UI for Daria to chat with her freinds',
+        cohort: 'node',
+        level: 'Beginner',
+        option: 'Required'
+    },
+    {
+        image: Project3,
+        title: 'Game',
+        desc: 'This is a UI build with css and node for Beginners. This is a showcase of all the projects that a person will be building',
+        cohort: 'node',
+        level: 'Beginner',
+        option: 'Elective'
+    },
+    {
+        image: Project3,
+        title: 'Game',
+        desc: 'This is a UI build with css and html for Beginners. This is a showcase of all the projects that a person will be building',
+        cohort: 'react',
+        level: 'Beginner',
+        option: 'Elective'
+    },
+    {
+        image: Project6,
+        title: 'Social Media',
+        desc: 'This is a UI build with css and react for Beginners. This is a black mode css and react web project for advance users',
+        cohort: 'react',
+        level: 'Advance',
+        option: 'Required'
+    },
+    {
+        image: Project5,
+        title: 'Dashboard',
+        desc: 'This is a UI build with css and react for Beginners. Here we will be making real engines that is use for displaying real data',
+        cohort: 'react',
+        level: 'Advance',
+        option: 'Required'
+    },
+]
