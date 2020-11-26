720e92e27e6831d834f84511a6c3fa4c7bdda261
Ajmal Jalal <ajmal@zennify.com>
2020-11-25 17:30:51 -0800

    projects page work in progress

--- src/containers/Feature.jsx
+++ src/containers/Feature.jsx
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 
 export default class Feature extends Component {
   setScreen = (screen) => {
-    const { screenChangerFunc } = this.props 
+    const { screenChangerFunc } = this.props
     screenChangerFunc(screen)
   }
   render() {
-    
+
     return (
       <div className='feature'>
         <div className='feature__text'>
-          <h1 className='feature__text__header'>Learn web developement with projects by real people</h1>
+          <h1 className='feature__text__header'>Learn web development with real world projects</h1>
+          <div className='feature__divider' />
           <p className='feature__text__desc'>Web developement/coding is made easy by building tons of real world projects with enterprise level professionals. This is not just a coding bootcamp but a 4 months intense working experience that prepares you for software engineer roles in the real world.</p>
-          <button className='feature__text__btn' onClick={()=>this.setScreen('register')}>REGISTER NOW</button>
-          <button className='feature__text__btn feature__text__btn--outlined' onClick={()=>this.setScreen('syllabus')}>VIEW SYLLABUS</button>
+          <button className='feature__text__btn' onClick={() => this.setScreen('register')}>Apply Now</button>
+          <button className='feature__text__btn feature__text__btn--outlined' onClick={() => this.setScreen('syllabus')}>Learn More</button>
         </div>
       </div>
     )
--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -4,7 +4,27 @@ export default class Projects extends Component {
   render() {
     return (
       <div className='projects-w'>
-        projects
+        <div className='projects__header'>
+          <div className='projects__header__btns-w'>
+            <div className='projects__header__btn'>HTML & CSS</div>
+            <div className='projects__header__btn'>Vanila JavaScript</div>
+            <div className='projects__header__btn'>React & Redux</div>
+            <div className='projects__header__btn'>Node, Express & Databases</div>
+          </div>
+          <div>list view</div>
+        </div>
+        <div className='projects__list'>
+          <div className='projects__list-item'>
+            <div></div>
+          </div>
+          <div className='projects__list-item'>One</div>
+          <div className='projects__list-item'>One</div>
+          <div className='projects__list-item'>One</div>
+          <div className='projects__list-item'>One</div>
+          <div className='projects__list-item'>One</div>
+          <div className='projects__list-item'>One</div>
+          <div className='projects__list-item'>One</div>
+        </div>
       </div>
     )
   }
