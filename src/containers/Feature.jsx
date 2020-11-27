8dd5c1cea226c0835bb670969d81974550439c2f
Ajmal Jalal <ajmal@zennify.com>
2020-11-26 17:57:14 -0800

    finished some design changes and spelling issues

--- src/containers/Feature.jsx
+++ src/containers/Feature.jsx
@@ -12,7 +12,7 @@ export default class Feature extends Component {
         <div className='feature__text'>
           <h1 className='feature__text__header'>Become a web developer by building real world projects</h1>
           <div className='feature__divider' />
-          <p className='feature__text__desc'>Wit CodeExpert, by building dozens of real world projects with enterprise level professionals, programing makes much more sense</p>
+          <p className='feature__text__desc'>With CodeExpert, by building dozens of real world projects with enterprise level professionals, programing makes much more sense</p>
           <button className='feature__text__btn' onClick={() => this.setScreen('register')}>Apply Now</button>
           <button
             onClick={() => document.getElementById('#more').scrollIntoView(
